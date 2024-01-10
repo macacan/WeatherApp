095a42332144b2d9260bf6fbc8c1dfcc22807dd7
Martin <can_martin14@hotmail.com>
2024-01-10 18:21:50 +0100

    fixat det fundementala

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,51 +1,55 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 const api = {
   key: "db87f278d9f51e2ce2523b851566af30",
-  base:"https://api.openweathermap.org/data/2.5/"}
-
-function App = () => {
-
-  
-    return (
-      <>
-        <h1>Väder applikation</h1>
-  
-        <input type="text" placeholder='Sök' />
-  
-        <p>Stockholm, Sverige</p>
-  
-        <p>temperatur 20C</p>
-  
-
-
-        
-        <p>soligt</p>
-        </>
-  );
+  base: "https://api.openweathermap.org/data/2.5/",
 };
-=======
-import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather, SetWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        SetWeather(result);
+        console.log(result);
+      });
+  };
+
   return (
-    <>
-      <h1>Väder applikation</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Väderkoll</h1>
 
-      <input type="text" placeholder="Sök" />
+        <div>
+          <input
+            type="text"
+            placeholder="Sök..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
 
-      <p>Stockholm, Sverige</p>
+          <button onClick={searchPressed}>Sök</button>
+        </div>
+        {weather && weather.name && (
+          <div>
+            <p>{weather.name}</p>
+            <p>{weather.main.temp}°C</p>
+            <p>{weather.timezone}</p>
 
-      <p>temperatur 20C</p>
-
-      <p>soligt</p>
-    </>
+            <div>
+              <p>{weather.weather[0].main}</p>
+              <p>{weather.weather[0].description}</p>
+            </div>
+          </div>
+        )}
+      </header>
+    </div>
   );
 }
->>>>>>> main
 
 export default App;

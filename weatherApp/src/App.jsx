88a82ea1b9a89d78ac15baf71962b1ebf0f24525
Martin <can_martin14@hotmail.com>
2024-01-10 15:27:01 +0100

    merge

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -3,33 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = {
+  key: "db87f278d9f51e2ce2523b851566af30",
+  base:"https://api.openweathermap.org/data/2.5/"}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+function App = () => {
+
+  
+    return (
+      <>
+        <h1>Väder applikation</h1>
+  
+        <input type="text" placeholder='Sök' />
+  
+        <p>Stockholm, Sverige</p>
+  
+        <p>temperatur 20C</p>
+  
+
+
+        
+        <p>soligt</p>
+        </>
+  );
+};
 
 export default App

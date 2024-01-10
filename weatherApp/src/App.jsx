3c25c19b4513e7206148f83b784ada7eae30575e
Martin <can_martin14@hotmail.com>
2024-01-10 15:32:11 +0100

    lagt till lite h1 input p

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      
+      <h1>Väder applikation</h1>
+
+      <input type="text" placeholder="Sök" />
+
+      <p>Stockholm, Sverige</p>
+
+      <p>temperatur 20C</p>
+
+      <p>soligt</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
